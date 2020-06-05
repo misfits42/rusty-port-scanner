f33aae32a81530ad49164577fc64c33de3aa2868
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-06-05 14:55:38 +1000

    Implemented multi-threading for localhost TCP port scanner

---
+++ README.md
@@ -0,0 +1,19 @@
+# Rusty Port Scanner
+
+A port scanner written in Rust as a personal project.
+
+I plan to add more useful features to the tool, such as:
+- UDP port scanning
+- Service discovery
+- Multiple-host scanning (subnets)
+
+## Compilation for Windows (on Linux dev box)
+
+1. Add the Rust toolchain "rustup target add x86_64-pc-windows-gnu"
+   1. `rustup add target x86_64-pc-windows-gnu`
+2. Install "mingw-w64" linker
+   1. `sudo apt install mingw-w64`
+3. Compile program targeting Windows:
+   1. `cargo build --target x86_64-pc-windows-gnu --release`
+4. Locate Windows x86_64 binary in project directory at "target/x86_64-pc-windows-gnu/release/rusty-port-scanner.exe"
+5. Run tool on Windows and enjoy!
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use std::time::Duration;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream};
+use std::thread;
+use std::time::Duration;
+
+const THREAD_CAP: u16 = 100;
 
 fn main() {
     scan_localhost_tcp_ports();
@@ -7,10 +10,32 @@ fn main() {
 
 /// Conducts a port scan on local computer
 fn scan_localhost_tcp_ports() {
-    for port in 1..=65535 {
-        let socket_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
-        if let Ok(_stream) = TcpStream::connect_timeout(&socket_addr, Duration::from_millis(5)) {
-            println!("connected to {}", &socket_addr);
-        }
+    let mut worker_threads: Vec<thread::JoinHandle<()>> = vec![];
+    // Spawn all the worker threads
+    for i in 1..THREAD_CAP + 1 {
+        worker_threads.push(thread::spawn(move || {
+            // Calculate the start and end points to cover entire range of ports over all threads
+            let start_port = (65535 / (THREAD_CAP - 1)) * (i - 1) + 1;
+            let end_port = {
+                if (65535 - start_port) < (65535 / (THREAD_CAP - 1)) {
+                    65535
+                } else {
+                    start_port + (65535 / (THREAD_CAP - 1)) - 1
+                }
+            };
+            // Scan all TCP ports allocated to the thread
+            for port in start_port..=end_port {
+                let socket_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
+                if let Ok(_stream) =
+                    TcpStream::connect_timeout(&socket_addr, Duration::from_millis(10))
+                {
+                    println!("[+] Connected to {}", &socket_addr);
+                }
+            }
+        }));
+    }
+    // Join the worker threads
+    for worker in worker_threads {
+        let _ = worker.join();
     }
 }
