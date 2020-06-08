8645b2669a6008cceab9de8e5104d150b2e8ec08
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-06-08 22:12:24 +1000

    Added port range specification for single-host TCP connect scan

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,116 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream};
-use std::thread;
 use std::sync::{Arc, Mutex};
+use std::thread;
 use std::time::Duration;
+use std::cmp;
+use std::collections::HashMap;
 
-use clap::{Arg, App};
+use clap::{App, Arg};
 use indicatif::{ProgressBar, ProgressStyle};
 
-const THREAD_CAP: u16 = 100;
+const THREAD_CAP: u16 = 10;
 
 fn main() {
     // Set up command-line argument parser
     let matches = App::new("Rusty Port Scanner")
-                        .version("0.1.0")
-                        .about("TCP and UDP port scanner")
-                        .author("Connor Mooney-Collett")
-                        .arg(Arg::with_name("target")
-                            .short("T")
-                            .long("target")
-                            .value_name("TARGET")
-                            .help("Scan target")
-                            .takes_value(true))
-                        .arg(Arg::with_name("timeout")
-                            .short("t")
-                            .long("timeout")
-                            .value_name("TIMEOUT")
-                            .help("Timeout for TCP connection in ms")
-                            .takes_value(true))
-                        .get_matches();
+        .version("0.1.0")
+        .about("TCP and UDP port scanner")
+        .author("Connor Mooney-Collett")
+        .arg(
+            Arg::with_name("target")
+                .short("T")
+                .long("target")
+                .value_name("TARGET")
+                .help("Scan target")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("timeout")
+                .short("t")
+                .long("timeout")
+                .value_name("TIMEOUT")
+                .help("Timeout for TCP connection in ms")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("ports")
+                .short("p")
+                .long("ports")
+                .value_name("PORTS")
+                .help("Range of ports to scan")
+                .takes_value(true),
+        )
+        .get_matches();
     // Extract command-line arguments
     let target = matches.value_of("target").unwrap_or("127.0.0.1");
-    let timeout_ms = matches.value_of("timeout").unwrap_or("0").parse::<u64>().unwrap();
+    let timeout_ms = matches
+        .value_of("timeout")
+        .unwrap_or("0")
+        .parse::<u64>()
+        .unwrap();
+    // Generate the port range to use
+    let port_limits_raw = matches
+        .value_of("ports")
+        .unwrap_or("1-1024")
+        .split("-")
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>();
+    if port_limits_raw.len() > 2 {
+        eprintln!("[!] ERROR: Invalid number of port limits. Please provide single port, or lower and upper limit.");
+        return;
+    }
+    let mut port_limits: Vec<u16> = vec![];
+    for raw in port_limits_raw {
+        // Check if the string represents a valid u16 value
+        let convert = raw.parse::<u16>();
+        if convert.is_err() {
+            eprintln!("[!] ERROR: Invalid port limit format.");
+            return;
+        }
+        // Check if 0 was given
+        let convert = convert.unwrap();
+        if convert == 0 {
+            eprintln!("[!] ERROR: Lower port limit cannot be 0.");
+            return;
+        }
+        port_limits.push(convert);
+    }
+    // Check that the lower limit is less than or equal to upper limit
+    if port_limits.len() == 2 && (port_limits[0] > port_limits[1]) {
+        eprintln!("[!] ERROR: Lower port limit must be less than or equal to upper limit.");
+        return;
+    }
+    // Generate range of ports to scan
+    let port_range: Vec<u16> = {
+        if port_limits.len() == 1 {
+            vec![port_limits[0]]
+        } else {
+            (port_limits[0]..=port_limits[1]).collect()
+        }
+    };
     // TEST - scan localhost TCP ports
-    scan_host_tcp_ports(String::from(target), timeout_ms);
+    scan_host_tcp_ports(String::from(target), timeout_ms, port_range);
 }
 
 /// Conducts a port scan on local computer
-fn scan_host_tcp_ports(target: String, timeout_ms: u64) {
+fn scan_host_tcp_ports(target: String, timeout_ms: u64, ports: Vec<u16>) {
     println!("Scanning {} ...", target);
     // Initialise array to hold handles to worker threads
     let mut worker_threads: Vec<thread::JoinHandle<()>> = vec![];
     // Initialise variables to be shared across threads
-    let progress_bar = Arc::new(Mutex::new(ProgressBar::new(65535)));
-    progress_bar.lock().unwrap().set_style(ProgressStyle::default_bar()
-        .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta})")
-        .progress_chars("#>-"));
-    let ports_open = Arc::new(Mutex::new(Vec::<u16>::new()));
+    let progress_bar = Arc::new(Mutex::new(ProgressBar::new(ports.len() as u64)));
+    progress_bar.lock().unwrap().set_style(
+        ProgressStyle::default_bar()
+            .template(
+                "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta})",
+            )
+            .progress_chars("#>-"),
+    );
+    let ports_open = Arc::new(Mutex::new(HashMap::<u16, String>::new()));
+    let ports_to_scan = Arc::new(Mutex::new(ports.clone()));
+    let total_ports: u16 = ports.len() as u16;
+    // Calculate number of threads to use
+    let total_threads = cmp::min(total_ports, THREAD_CAP);
     // Check if IP address provided is valid
     let ip_addr_result = target.parse::<Ipv4Addr>();
     if ip_addr_result.is_err() {
@@ -53,22 +119,24 @@ fn scan_host_tcp_ports(target: String, timeout_ms: u64) {
     }
     let ip_addr = IpAddr::V4(ip_addr_result.unwrap());
     // Spawn all the worker threads
-    for i in 1..THREAD_CAP + 1 {
+    for i in 0..total_threads {
         // Clone the shared variables
         let progress_bar = Arc::clone(&progress_bar);
         let ports_open = Arc::clone(&ports_open);
+        let ports_to_scan = Arc::clone(&ports_to_scan);
         worker_threads.push(thread::spawn(move || {
             // Calculate the start and end points to cover entire range of ports over all threads
-            let start_port = (65535 / (THREAD_CAP - 1)) * (i - 1) + 1;
-            let end_port = {
-                if (65535 - start_port) < (65535 / (THREAD_CAP - 1)) {
-                    65535
+            let start_index = ((total_ports / total_threads) + 1) * i;
+            let end_index = {
+                if (total_ports - start_index) < (total_ports / (total_threads)) {
+                    total_ports - 1
                 } else {
-                    start_port + (65535 / (THREAD_CAP - 1)) - 1
+                    start_index + (total_ports / total_threads)
                 }
             };
             // Scan all TCP ports allocated to the thread
-            for port in start_port..=end_port {
+            for port_index in start_index..=end_index {
+                let port = ports_to_scan.lock().unwrap()[port_index as usize];
                 // Create the new connect for connection attempt
                 let socket_addr = SocketAddr::new(ip_addr, port);
                 // Attempt TCP connection with timeout if specified
@@ -81,7 +149,10 @@ fn scan_host_tcp_ports(target: String, timeout_ms: u64) {
                 };
                 // Check if the connection attempt was successful or not
                 if connect_result.is_ok() {
-                    ports_open.lock().unwrap().push(port);
+                    // TODO: Conduct banner grab
+
+                    // Record port as being open
+                    ports_open.lock().unwrap().insert(port, String::from(""));
                 }
                 // Increment the total number of ports scanned
                 let pb = progress_bar.lock().unwrap();
@@ -97,9 +168,10 @@ fn scan_host_tcp_ports(target: String, timeout_ms: u64) {
     let pb = progress_bar.lock().unwrap();
     pb.finish();
     // Unwrap the Arc and mutex to get the ports opened
-    let mut ports_open = Arc::try_unwrap(ports_open).unwrap().into_inner().unwrap();
-    ports_open.sort();
-    for port in ports_open.iter() {
-        println!("[+] OPEN - tcp/{}", port);
+    let ports_open = Arc::try_unwrap(ports_open).unwrap().into_inner().unwrap();
+    let mut ordered_ports_open = ports_open.keys().map(|x| *x).collect::<Vec<u16>>();
+    ordered_ports_open.sort();
+    for (port, banner) in ports_open {
+        println!("[+] OPEN - tcp/{} - {}", port, banner);
     }
 }
